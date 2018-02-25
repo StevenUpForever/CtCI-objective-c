a909d31e8b49d499dc561347a2d924da1504ea07
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-02-24 20:16:55 -0800

    initial upload

---
+++ CtCI-objective-c/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CtCI-objective-c
+//
+//  Created by Chengzhi Jia on 2/24/18.
+//  Copyright © 2018 Chengzhi Jia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
